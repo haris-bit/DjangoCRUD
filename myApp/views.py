7a384bfb4158f9c826108d27f638af4f64dd2b87
Haris <harisiftikhar109@gmail.com>
2023-09-06 01:39:36 +0500

    initials

---
+++ myApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Hello")
+
+
+def products(request):
+    return HttpResponse("Products")
+
+
+def customer(request):
+    return HttpResponse("customer")
