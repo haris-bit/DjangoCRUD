236f9bce09a5aed36469cc0b7d210de531f6c443
haris-bit <bsef19m528@pucit.edu.pk>
2023-12-28 14:54:48 +0500

    leaderboard-list API cached

--- myApp/views.py
+++ myApp/views.py
@@ -251,95 +251,109 @@ def get_correct_picks(request, format=None):
 
 
 
+# Decorate the view with caching
 @require_http_methods(["GET", "OPTIONS"])  # Allow GET and OPTIONS requests
 @api_view(["GET"])
+@cache_page(60 * 15)  # Cache for 15 minutes
 def leaderboard_list(request, format=None):
-    if request.method == 'GET':
-        try:
-            # Make a GET request to the JSON URL to fetch user data
-            url = "https://sheetdb.io/api/v1/3z14mlm79tmet"
-            response = requests.get(url)
+    # Check if the result is already cached
+    cached_result = cache.get('leaderboard_list')
+    if cached_result:
+        return Response(cached_result)
+    else:
+        if request.method == 'GET':
+            try:
+                # Make a GET request to the JSON URL to fetch user data
+                url = "https://sheetdb.io/api/v1/3z14mlm79tmet"
+                response = requests.get(url)
 
-            # Check if the request was successful (status code 200)
-            if response.status_code == 200:
-                json_data = response.json()
+                # Check if the request was successful (status code 200)
+                if response.status_code == 200:
+                    json_data = response.json()
 
-                # Initialize dictionaries to store player_per and sum_per_actual data
-                player_per_cache = {}  # Cache player_per data
-                sum_per_actual_cache = {}  # Cache sum_per_actual data
+                    # Initialize dictionaries to store player_per and sum_per_actual data
+                    player_per_cache = {}  # Cache player_per data
+                    sum_per_actual_cache = {}  # Cache sum_per_actual data
 
-                # Initialize a list to store leaderboard data for all users
-                leaderboard_data_list = []
+                    # Initialize a list to store leaderboard data for all users
+                    leaderboard_data_list = []
 
-                for user_data in json_data:
-                    username = user_data.get("Username", None)
+                    for user_data in json_data:
+                        username = user_data.get("Username", None)
 
-                    if username:
-                        # Fetch player names from user data (Who_1 to Who_20)
-                        player_names = [user_data.get(f'Who_{i}', None) for i in range(1, 21)]
+                        if username:
+                            # Fetch player names from user data (Who_1 to Who_20)
+                            player_names = [user_data.get(f'Who_{i}', None) for i in range(1, 21)]
 
-                        # Fetch player efficiency ratings for the user
-                        if username not in player_per_cache:
-                            player_per_api_url = f"https://mybackendnba-e0bd8ae9accb.herokuapp.com/api/player_efficiency_ratings/{username}/"
-                            player_per_response = requests.get(player_per_api_url)
+                            # Fetch player efficiency ratings for the user
+                            if username not in player_per_cache:
+                                player_per_api_url = f"https://mybackendnba-e0bd8ae9accb.herokuapp.com/api/player_efficiency_ratings/{username}/"
+                                player_per_response = requests.get(player_per_api_url)
 
-                            if player_per_response.status_code == 200:
-                                player_per_data = player_per_response.json()
-                                player_per_dict = {item["player_name"]: item["per"] for item in player_per_data}
-                                player_per_cache[username] = player_per_dict
-                            else:
-                                return Response({'detail': 'Failed to fetch player_per data'}, status=500)
+                                if player_per_response.status_code == 200:
+                                    player_per_data = player_per_response.json()
+                                    player_per_dict = {item["player_name"]: item["per"] for item in player_per_data}
+                                    player_per_cache[username] = player_per_dict
+                                else:
+                                    return Response({'detail': 'Failed to fetch player_per data'}, status=500)
 
-                        player_per_dict = player_per_cache[username]
+                            player_per_dict = player_per_cache[username]
 
-                        # Fetch actual player statistics using the existing API
-                        if "sum_per_actual" not in sum_per_actual_cache:
-                            sum_per_actual_api_url = "https://mybackendnba-e0bd8ae9accb.herokuapp.com/advance-stats/"
-                            sum_per_actual_response = requests.get(sum_per_actual_api_url)
+                            # Fetch actual player statistics using the existing API
+                            if "sum_per_actual" not in sum_per_actual_cache:
+                                sum_per_actual_api_url = "https://mybackendnba-e0bd8ae9accb.herokuapp.com/advance-stats/"
+                                sum_per_actual_response = requests.get(sum_per_actual_api_url)
 
-                            if sum_per_actual_response.status_code == 200:
-                                sum_per_actual_data = sum_per_actual_response.json()
-                                top_20_players = [item["player_efficiency_rating"] for item in sum_per_actual_data]
-                                sum_per_actual_cache["sum_per_actual"] = sum(top_20_players)
-                            else:
-                                return Response({'detail': 'Failed to fetch sum_per_actual data'}, status=500)
+                                if sum_per_actual_response.status_code == 200:
+                                    sum_per_actual_data = sum_per_actual_response.json()
+                                    top_20_players = [item["player_efficiency_rating"] for item in sum_per_actual_data]
+                                    sum_per_actual_cache["sum_per_actual"] = sum(top_20_players)
+                                else:
+                                    return Response({'detail': 'Failed to fetch sum_per_actual data'}, status=500)
 
-                        sum_per_actual = sum_per_actual_cache["sum_per_actual"]
+                            sum_per_actual = sum_per_actual_cache["sum_per_actual"]
 
-                        # Calculate sum PER user submitted and correct picks count
-                        sum_per_user_submitted = 0
-                        correct_picks_count = 0
+                            # Calculate sum PER user submitted and correct picks count
+                            sum_per_user_submitted = 0
+                            correct_picks_count = 0
 
-                        for i, player_name in enumerate(player_names):
-                            if player_name and player_name != 'null':
-                                player_per = player_per_dict.get(player_name, None)
-                                if player_per is not None:
-                                    sum_per_user_submitted += player_per
+                            for i, player_name in enumerate(player_names):
+                                if player_name and player_name != 'null':
+                                    player_per = player_per_dict.get(player_name, None)
+                                    if player_per is not None:
+                                        sum_per_user_submitted += player_per
 
-                                # Check if the pick matches the corresponding player in the top 20
-                                if i < len(top_20_players) and player_per == top_20_players[i]:
-                                    correct_picks_count += 1
+                                    # Check if the pick matches the corresponding player in the top 20
+                                    if i < len(top_20_players) and player_per == top_20_players[i]:
+                                        correct_picks_count += 1
 
-                        # Calculate Accuracy
-                        accuracy = (correct_picks_count / 20) * 100
+                            # Calculate Accuracy
+                            accuracy = (correct_picks_count / 20) * 100
 
-                        # Add the leaderboard data to the list
-                        leaderboard_data = {
-                            'Username': username,
-                            'sum_per_user_submitted': sum_per_user_submitted,
-                            'sum_per_actual': sum_per_actual,
-                            'per_percentage': (sum_per_user_submitted / sum_per_actual) * 100,
-                            'correct_picks_count': correct_picks_count,
-                            'accuracy': accuracy
-                        }
-                        leaderboard_data_list.append(leaderboard_data)
+                            # Add the leaderboard data to the list
+                            leaderboard_data = {
+                                'Username': username,
+                                'sum_per_user_submitted': sum_per_user_submitted,
+                                'sum_per_actual': sum_per_actual,
+                                'per_percentage': (sum_per_user_submitted / sum_per_actual) * 100,
+                                'correct_picks_count': correct_picks_count,
+                                'accuracy': accuracy
+                            }
+                            leaderboard_data_list.append(leaderboard_data)
 
-                # Return the list of leaderboard data as a JSON response
-                return Response(leaderboard_data_list)
-            else:
-                return Response({'detail': 'Failed to fetch JSON data'}, status=response.status_code)
-        except Exception as e:
-            return Response({'detail': str(e)}, status=500)
+                    # Sort the leaderboard data based on a suitable metric
+                    leaderboard_data_list = sorted(leaderboard_data_list, key=lambda x: (-x['correct_picks_count'], -x['sum_per_user_submitted'], -x['per_percentage']))
+
+                    # Cache the result
+                    cache.set('leaderboard_list', leaderboard_data_list, 60 * 15)  # Cache for 15 minutes
+
+                    # Return the sorted list of leaderboard data as a JSON response
+                    return Response(leaderboard_data_list)
+                else:
+                    return Response({'detail': 'Failed to fetch JSON data'}, status=response.status_code)
+            except Exception as e:
+                return Response({'detail': str(e)}, status=500)
+
 
 
 
